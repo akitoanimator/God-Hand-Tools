b25a91eb515c3a3ade8a1751c7ed038fe4fe080e
akitoanimator <144627279+akitoanimator@users.noreply.github.com>
2023-09-22 17:48:55 -0300

    Delete assembly directory

--- assembly/linkedMesh.cs
+++
@@ -1,8 +0,0 @@
-using System;
-using System.Collections.Generic;
-using System.IO;
-
-public class linkedMesh
-{
-    public int[] offsets;
-}
--- assembly/ultility/Vertex.cs
+++
@@ -1,9 +0,0 @@
-using System;
-using System.Collections.Generic;
-using System.Text;
-using System.IO;
-
-public class Vertex
-{
-    public float[] pos = { 0,0,0};
-}
